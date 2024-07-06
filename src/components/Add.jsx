a967955ac6a250fb4a1a44128dfd4329fef22ab7
mridular <mridula.r@ictkerala.org>
2024-07-06 10:32:53 +0530

    first commit

---
+++ src/components/Add.jsx
@@ -0,0 +1,78 @@
+import { Box, Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
+
+const Add = ({person}) => {
+  const [page,setPage]=useState('Movie')
+  const [count,setCount]=useState(0);
+  const [form,setForm]=useState({
+    name:person.name,
+    email:person.email,
+    password:person.password,
+    address:person.address
+  })
+  function valueAdd() {
+    // setCount(count+1)
+    console.log(form)
+  }
+  function valueCap(e) {
+    // console.log(e)
+    setForm({...form,[e.target.name]:e.target.value})
+  }
+  return (
+    <Box
+    component="form"
+    sx={{
+      '& .MuiTextField-root': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <h1>Welcome to {page} Page</h1>
+    <div>
+      <TextField
+        required
+        id="outlined-required"
+        label="Name"
+       name='name'
+       value={form.name}
+       onChange={valueCap}
+      />
+      </div>
+      <div>
+      <TextField
+    
+        id="outlined-disabled"
+        label="Email"
+        type='email'
+        name='email'
+        value={form.email}
+      />
+      </div>
+      <div>
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        name='password'
+        value={form.password}
+      />
+      </div>
+      <div>
+      <TextField
+        id="outlined-read-only-input"
+        label="Address"
+      name='address'
+      value={form.address}
+      />
+      </div>
+      <div>
+        <Button variant='contained' onClick={valueAdd}>Register</Button>
+        <br></br>
+      {/* <small>Button is clicked {count} times</small> */}
+      </div>
+      </Box>
+  )
+}
+
+export default Add
---
+++ src/components/View.jsx
@@ -0,0 +1,51 @@
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const View = () => {
+    // const rows=[{name:'Maya',email:'mr@gmail.com',phone:787282772},
+    //     {name:'Jiya',email:'ji@gmail.com',phone:7878787900},
+    //     {name:'Miya',email:'ghidi@gmail.com',phone:7888888888}
+    // ]
+    const [rows,setRows]=useState([])
+    useEffect(()=>{
+    axios.get('https://dummyjson.com/users').then((res)=>{
+      // console.log(res.data.users)
+      setRows(res.data.users)
+    })
+    },[])
+  return (
+
+    <div>
+         <TableContainer style={{marginTop:'7%'}} component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Phone</TableCell>
+           
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.firstName}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.firstName}
+              </TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.phone}</TableCell>
+             
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </div>
+  )
+}
+
+export default View
